a1b2687f2ab725984c69f585ae716a45198fbe63
Shreyas Bhandare <bhandare.shreyas@gmail.com>
2024-07-20 22:26:38 -0400

    started sleep actions

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -3,10 +3,17 @@ import { View, StyleSheet, Animated } from "react-native"
 import { Text, Icon, Button, Snackbar } from "react-native-paper"
 
 export default () => {
-    const [feedingCount, setFeedingCount] = useState(0);
     const [snackbarVisible, setSnackbarVisible] = useState(false);
     const [snackbarMessage, setSnackbarMessage] = useState('');
+
+    const [feedingCount, setFeedingCount] = useState(0);
     const animatedValue = useRef(new Animated.Value(0)).current;
+    
+    const [poopDiaperCount, setPoopDiaperCount] = useState(0);
+    const poopAnimatedValue = useRef(new Animated.Value(0)).current;
+
+    const [wetDiaperCount, setWetDiaperCount] = useState(0);
+    const wetAnimatedValue = useRef(new Animated.Value(0)).current;
 
     const updateFeedingCount = useCallback((step) => {
         setFeedingCount(prevCount => {
@@ -17,6 +24,40 @@ export default () => {
                 return prevCount;
             } else if (newCount > 100) {
                 setSnackbarMessage('Cannot exceed 100');
+                setSnackbarVisible(true);
+                return prevCount;
+            } else {
+                return newCount;
+            }
+        });
+    }, []);
+
+    const updatePoopDiaperCount = useCallback((step) => {
+        setPoopDiaperCount(prevCount => {
+            const newCount = prevCount + step;
+            if (newCount < 0) {
+                setSnackbarMessage('Cannot go below 0');
+                setSnackbarVisible(true);
+                return prevCount;
+            } else if (newCount > 30) {
+                setSnackbarMessage('Cannot exceed 30');
+                setSnackbarVisible(true);
+                return prevCount;
+            } else {
+                return newCount;
+            }
+        });
+    }, []);
+
+    const updateWetDiaperCount = useCallback((step) => {
+        setWetDiaperCount(prevCount => {
+            const newCount = prevCount + step;
+            if (newCount < 0) {
+                setSnackbarMessage('Cannot go below 0');
+                setSnackbarVisible(true);
+                return prevCount;
+            } else if (newCount > 30) {
+                setSnackbarMessage('Cannot exceed 30');
                 setSnackbarVisible(true);
                 return prevCount;
             } else {
@@ -49,6 +90,62 @@ export default () => {
         transform: [
             {
                 rotateX: interpolatedRotateAnimation,
+            },
+        ],
+    };
+
+    useEffect(() => {
+        Animated.sequence([
+            Animated.timing(poopAnimatedValue, {
+                toValue: 1,
+                duration: 75,
+                useNativeDriver: true,
+            }),
+            Animated.timing(poopAnimatedValue, {
+                toValue: 0,
+                duration: 75,
+                useNativeDriver: true,
+            }),
+        ]).start();
+    }, [poopDiaperCount]);
+
+    const poopRotateAnimation = poopAnimatedValue.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['0deg', '180deg'],
+    });
+
+    const poopAnimatedStyle = {
+        transform: [
+            {
+                rotateX: poopRotateAnimation,
+            },
+        ],
+    };
+
+    useEffect(() => {
+        Animated.sequence([
+            Animated.timing(wetAnimatedValue, {
+                toValue: 1,
+                duration: 75,
+                useNativeDriver: true,
+            }),
+            Animated.timing(wetAnimatedValue, {
+                toValue: 0,
+                duration: 75,
+                useNativeDriver: true,
+            }),
+        ]).start();
+    }, [wetDiaperCount]);
+
+    const wetRotateAnimation = wetAnimatedValue.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['0deg', '180deg'],
+    });
+
+    const wetAnimatedStyle = {
+        transform: [
+            {
+                rotateX: wetRotateAnimation,
             },
         ],
     };
@@ -113,6 +210,27 @@ export default () => {
                         size={35}
                     />
                 </View>
+                <View style={styles.poopActionsContainer}>
+                    <Button mode="contained" onPress={() => updatePoopDiaperCount(-1)}
+                        labelStyle={{ fontSize: 20, marginHorizontal: 0 }}
+                        contentStyle={{ width: 50, height: 60, paddingLeft: 10 }} >
+                        -1
+                    </Button>
+                    <Animated.View style={poopAnimatedStyle}>
+                        <Text variant="headlineMedium" style={{
+                            width: 80,
+                            textAlign: 'center',
+                            fontWeight: 'bold'
+                        }}>
+                            {formatCount(poopDiaperCount)}
+                        </Text>
+                    </Animated.View>
+                    <Button mode="contained" onPress={() => updatePoopDiaperCount(1)}
+                        labelStyle={{ fontSize: 20, marginHorizontal: 0 }}
+                        contentStyle={{ width: 50, height: 60, paddingLeft: 10 }} >
+                        +1
+                    </Button>
+                </View>
             </View>
             <View style={[styles.wetDiaperContainer]}>
                 <View style={styles.sectionTitle}>
@@ -121,6 +239,27 @@ export default () => {
                         source="weather-rainy"
                         size={35}
                     />
+                </View>
+                <View style={styles.peeActionsContainer}>
+                    <Button mode="contained" onPress={() => updateWetDiaperCount(-1)}
+                        labelStyle={{ fontSize: 20, marginHorizontal: 0 }}
+                        contentStyle={{ width: 50, height: 60, paddingLeft: 10 }} >
+                        -1
+                    </Button>
+                    <Animated.View style={wetAnimatedStyle}>
+                        <Text variant="headlineMedium" style={{
+                            width: 80,
+                            textAlign: 'center',
+                            fontWeight: 'bold'
+                        }}>
+                            {formatCount(wetDiaperCount)}
+                        </Text>
+                    </Animated.View>
+                    <Button mode="contained" onPress={() => updateWetDiaperCount(1)}
+                        labelStyle={{ fontSize: 20, marginHorizontal: 0 }}
+                        contentStyle={{ width: 50, height: 60, paddingLeft: 10 }} >
+                        +1
+                    </Button>
                 </View>
             </View>
             <View style={[styles.sleepContainer]}>
@@ -131,6 +270,37 @@ export default () => {
                         size={35}
                     />
                 </View>
+                <View style={styles.sleepActionsContainer}>
+                    <Button mode="contained" onPress={() => updateFeedingCount(-1)}
+                        labelStyle={{ fontSize: 16, marginHorizontal: 0 }}
+                        contentStyle={{ width: 60, height: 60, paddingLeft: 0 }} >
+                        -4Hr
+                    </Button>
+                    <Button mode="contained" onPress={() => updateFeedingCount(-0.5)}
+                        labelStyle={{ fontSize: 16, marginHorizontal: 0 }}
+                        contentStyle={{ width: 80, height: 60, paddingLeft: 0 }} >
+                        -30min
+                    </Button>
+                    <Animated.View style={animatedStyle}>
+                        <Text variant="headlineMedium" style={{
+                            width: 80,
+                            textAlign: 'center',
+                            fontWeight: 'bold'
+                        }}>
+                            {formatCount(feedingCount)}
+                        </Text>
+                    </Animated.View>
+                    <Button mode="contained" onPress={() => updateFeedingCount(0.5)}
+                        labelStyle={{ fontSize: 16, marginHorizontal: 0 }}
+                        contentStyle={{ width: 80, height: 60, paddingLeft: 0 }} >
+                        +30min
+                    </Button>
+                    <Button mode="contained" onPress={() => updateFeedingCount(1)}
+                        labelStyle={{ fontSize: 16, marginHorizontal: 0 }}
+                        contentStyle={{ width: 60, height: 60, paddingLeft: 0 }} >
+                        +4Hr
+                    </Button>
+                </View>
             </View>
             <Snackbar
                 visible={snackbarVisible}
@@ -139,8 +309,7 @@ export default () => {
                     label: 'Dismiss',
                     onPress: onDismissSnackbar,
                 }}
-                duration={3000}
-            >
+                duration={3000}>
                 {snackbarMessage}
             </Snackbar>
         </View>
@@ -187,15 +356,36 @@ const styles = StyleSheet.create({
         paddingHorizontal: 15,
         //backgroundColor: '#c11'
     },
+    poopActionsContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        //backgroundColor: '#ccc',
+    },
     wetDiaperContainer: {
         flex: 1,
         paddingHorizontal: 15,
         //backgroundColor: '#f12'
     },
+    peeActionsContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        //backgroundColor: '#ccc',
+    },
     sleepContainer: {
         flex: 1,
         paddingHorizontal: 15,
         //backgroundColor: '#d33'
+    },
+    sleepActionsContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        //backgroundColor: '#ccc',
     },
     sectionTitle: {
         flex: 1,
